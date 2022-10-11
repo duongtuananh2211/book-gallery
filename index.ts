814aeec20ccb6fb2fdb28310dd1685d1055836d5
anhdt2k1 <anhdt@agilead.vn>
2022-10-11 15:10:13 +0700

    Project init

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+const DEFAULT_PORT = "3000";
+const port = process.env.PORT || DEFAULT_PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
